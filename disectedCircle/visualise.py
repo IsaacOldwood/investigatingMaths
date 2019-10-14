968c34fefc882688a6e5a800c66701ea769e1869
Isaac Oldwood <isaac.oldwood@gmail.com>
2019-10-14 19:26:53 +0100

    Visulaisation for n=3

--- disectedCircle/visualise.py
+++ disectedCircle/visualise.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from random import random
 
-n=2 #Set n
+n=3 #Set n
 
 def point():
     #Generates a random point on the circle
@@ -13,6 +13,7 @@ def point():
 #Generate random points
 randomPoint_1=point()
 randomPoint_2=point()
+randomPoint_3=point()
 
 #Generate points on circle
 theta = np.linspace(0, 2*np.pi, 100)
@@ -25,6 +26,7 @@ x2 = r*np.sin(theta)
 #Rename random points
 x3 = (randomPoint_1[0],randomPoint_1[1])
 x4 = (randomPoint_2[0],randomPoint_2[1])
+x5 = (randomPoint_3[0],randomPoint_3[1])
 
 fig, ax = plt.subplots(1)
 
@@ -34,9 +36,10 @@ ax.plot(x1, x2)
 #Plot random Points
 ax.plot(x3[0],x3[1],marker='o',color='red')
 ax.plot(x4[0],x4[1],marker='o',color='red')
+ax.plot(x5[0],x5[1],marker='o',color='red')
 
 #Plot lines conecting random points
-ax.plot([x3[0],x4[0]],[x3[1],x4[1]])
+ax.plot([x3[0],x4[0],x5[0]],[x3[1],x4[1],x5[1]])
 
 ax.set_aspect(1)
 

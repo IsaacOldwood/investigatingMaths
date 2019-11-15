535cdc08b3558f525c2f7fc7d6a0879b3cef6cd0
Isaac Oldwood <isaac.oldwood@gmail.com>
2019-11-15 09:37:43 +0000

    Update visualise.py

--- disectedCircle/visualise.py
+++ disectedCircle/visualise.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from random import random
 
-n=3 #Set n
+n=4 #Set n
 
 def point():
     #Generates a random point on the circle
@@ -14,6 +14,7 @@ def point():
 randomPoint_1=point()
 randomPoint_2=point()
 randomPoint_3=point()
+randomPoint_4=point()
 
 #Generate points on circle
 theta = np.linspace(0, 2*np.pi, 100)
@@ -27,6 +28,7 @@ x2 = r*np.sin(theta)
 x3 = (randomPoint_1[0],randomPoint_1[1])
 x4 = (randomPoint_2[0],randomPoint_2[1])
 x5 = (randomPoint_3[0],randomPoint_3[1])
+x6 = (randomPoint_4[0],randomPoint_4[1])
 
 fig, ax = plt.subplots(1)
 
@@ -37,9 +39,10 @@ ax.plot(x1, x2)
 ax.plot(x3[0],x3[1],marker='o',color='red')
 ax.plot(x4[0],x4[1],marker='o',color='red')
 ax.plot(x5[0],x5[1],marker='o',color='red')
+ax.plot(x6[0],x6[1],marker='o',color='red')
 
 #Plot lines conecting random points
-ax.plot([x3[0],x4[0],x5[0]],[x3[1],x4[1],x5[1]])
+ax.plot([x3[0],x4[0],x5[0],x6[0]],[x3[1],x4[1],x5[1],x6[1]])
 
 ax.set_aspect(1)
 
